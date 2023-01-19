149e1a96dddbdbe6dde98a0dd62c95595dd1486c
MirkoFarina <MirkoFarina@outlook.com>
2023-01-19 16:44:11 +0100

    [add] migration and seeder in db

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Artist extends Model
 {
     use HasFactory;
+    public static function generator_slug ($title){
+        $slug = Str::slug($title,'-');
+        $original_slug = $slug;
+        $c = 1;
+
+        $slug_exists = Artist::where('slug', $slug)->first();
+
+        while($slug_exists) {
+            $slug = $original_slug . '-' . $c;
+            $slug_exists = Artist::where('slug', $slug)->first();
+            $c++;
+        }
+        return $slug;
+    }
 }
--- database/migrations/2023_01_19_144505_create_artists_table.php
+++ database/migrations/2023_01_19_144505_create_artists_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('artists', function (Blueprint $table) {
             $table->id();
+            $table->string('name',100);
+            $table->string('slug',110)->unique();
             $table->timestamps();
         });
     }
