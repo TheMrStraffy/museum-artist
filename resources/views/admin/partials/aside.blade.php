a038196ef3c0e98bc9dae39e858b8188020c0d32
SaverioCascelli <saverio.cascelli@gmail.com>
2023-01-19 17:23:43 +0100

    [add] style in aside

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,12 +1,22 @@
 <aside>
+
     <ul class="mt-5">
-        <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+        <a href="{{ route('admin.dashboard') }}">
+            <h5>Dashboard</h5>
+        </a>
+
+    </ul>
+
+    <ul class="mt-5">
+        <h5 class="text-white">Museum</h5>
         <li><a href="{{ route('admin.museum.index') }}">Index</a></li>
         <li><a href="{{ route('admin.museum.create') }}">Create</a></li>
         <li><a href=""></a></li>
         <li><a href=""></a></li>
     </ul>
+
     <ul class="mt-5">
+        <h5 class="text-white">Artists</h5>
         <li><a href="{{ route('admin.artist.index') }}">Index</a></li>
         <li><a href="{{ route('admin.artist.create') }}">Create</a></li>
         <li><a href=""></a></li>
