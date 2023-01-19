e37ab345f58547f86db32cd914204690eb5239ca
TheMrStraffy <carmelo.miuccio2@gmail.com>
2023-01-19 16:09:37 +0100

    [init] project museum-artist

---
+++ app/Http/Controllers/Admin/MuseumController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use app\http\Controllers\Controller;
+use App\Models\Museum;
+use App\Http\Requests\StoreMuseumRequest;
+use App\Http\Requests\UpdateMuseumRequest;
+
+class MuseumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreMuseumRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMuseumRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Museum  $museum
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Museum $museum)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Museum  $museum
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Museum $museum)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateMuseumRequest  $request
+     * @param  \App\Models\Museum  $museum
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMuseumRequest $request, Museum $museum)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Museum  $museum
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Museum $museum)
+    {
+        //
+    }
+}
