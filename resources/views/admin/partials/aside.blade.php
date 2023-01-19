abed6fb00085941665c1a860be694f799c56f0a1
SaverioCascelli <saverio.cascelli@gmail.com>
2023-01-19 17:05:18 +0100

    [add] route in list aside

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,8 +1,14 @@
 <aside>
     <ul class="mt-5">
         <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-        <li><a href="{{ route('admin..index') }}">Index</a></li>
-        <li><a href="{{ route('admin..create') }}">Create</a></li>
+        <li><a href="{{ route('admin.museums.index') }}">Index</a></li>
+        <li><a href="{{ route('admin.museums.create') }}">Create</a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+    </ul>
+    <ul class="mt-5">
+        <li><a href="{{ route('admin.artists.index') }}">Index</a></li>
+        <li><a href="{{ route('admin.artists.create') }}">Create</a></li>
         <li><a href=""></a></li>
         <li><a href=""></a></li>
     </ul>
