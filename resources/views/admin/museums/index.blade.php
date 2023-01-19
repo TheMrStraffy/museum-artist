058539c9718652056e99910bead0f67de0a85f29
mikzanco <mikyzancolo97@gmail.com>
2023-01-19 16:36:28 +0100

    esteso il layout.app nelle view museum, creato il section nelle views del museo

---
+++ resources/views/admin/museums/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title')
+    | Admin
+@endsection
+
+@section('content')
+<div class="container">
+    <h1>Elenco dei Musei</h1>
+
+
+      {{$xxx->links()}}
+</div>
+@endsection
