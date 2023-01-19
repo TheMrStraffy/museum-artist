7be993d4122a712f860b6009b202931e6efef3c5
SaverioCascelli <saverio.cascelli@gmail.com>
2023-01-19 17:01:42 +0100

    [add] aside list

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,9 @@
+<aside>
+    <ul class="mt-5">
+        <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+        <li><a href="{{ route('admin..index') }}">Index</a></li>
+        <li><a href="{{ route('admin..create') }}">Create</a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+    </ul>
+</aside>
