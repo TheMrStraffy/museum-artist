c8747d1741649e2e017ac638468847f631bd790d
SaverioCascelli <saverio.cascelli@gmail.com>
2023-01-19 17:12:17 +0100

    [fix] ref in aside

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,14 +1,14 @@
 <aside>
     <ul class="mt-5">
         <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-        <li><a href="{{ route('admin.museums.index') }}">Index</a></li>
-        <li><a href="{{ route('admin.museums.create') }}">Create</a></li>
+        <li><a href="{{ route('admin.museum.index') }}">Index</a></li>
+        <li><a href="{{ route('admin.museum.create') }}">Create</a></li>
         <li><a href=""></a></li>
         <li><a href=""></a></li>
     </ul>
     <ul class="mt-5">
-        <li><a href="{{ route('admin.artists.index') }}">Index</a></li>
-        <li><a href="{{ route('admin.artists.create') }}">Create</a></li>
+        <li><a href="{{ route('admin.artist.index') }}">Index</a></li>
+        <li><a href="{{ route('admin.artist.create') }}">Create</a></li>
         <li><a href=""></a></li>
         <li><a href=""></a></li>
     </ul>
